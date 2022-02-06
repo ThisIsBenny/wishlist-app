ea0be6fe69ac77a88adff0023263616d40a8184a
Benny Samir Hierl <bennysamir@posteo.de>
2022-02-06 14:40:37 +0100

    #4 api to load open graph metadata added
    Signed-off-by: Benny Samir Hierl <bennysamir@posteo.de>

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,11 +1,13 @@
 import { FastifyInstance } from 'fastify'
 import { default as wishlistRoute } from './wishlist/'
+import { default as utilsRoute } from './utils/'
 
 export default {
   register: (app: FastifyInstance) => {
     return app.register(
       async (app) => {
         await app.register(wishlistRoute, { prefix: '/wishlist' })
+        await app.register(utilsRoute, { prefix: '/utils' })
       },
       { prefix: '/api' }
     )
