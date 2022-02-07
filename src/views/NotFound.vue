3b842e960e6827f3b9572d63d96b972062c4b6a9
Benny Samir Hierl <bennysamir@posteo.de>
2022-02-07 20:39:52 +0100

    not found pages improved

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,7 +1,13 @@
 <template>
-  <h1>{{ t('errors.not-found.text') }}</h1>
+  <div
+    class="mt-20 flex flex-wrap flex-col sm:flex-row items-center sm:space-x-2 justify-center text-center sm:text-left text-xl"
+  >
+    <IconCloudQuestion class="h-12 w-12" />
+    <h1>{{ t('errors.not-found.text') }}</h1>
+  </div>
 </template>
 <script setup lang="ts">
+import { IconCloudQuestion } from '@/components/icons'
 import { useI18n } from 'vue-i18n'
 const { t } = useI18n()
 </script>
