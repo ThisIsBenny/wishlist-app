982562a65414503048ea5eef98ce164058f04c3d
Benny Hierl <benny.hierl@pm.me>
2026-03-02 17:22:36 +0100

    fix: exclude e2e tests from vitest and add globals option

---
+++ vitest.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'url'
+import Components from 'unplugin-vue-components/vite'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    Components({
+      dts: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  test: {
+    environment: 'jsdom',
+    exclude: [
+      '**/e2e/**',
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/playwright-report/**',
+      '**/test-results/**',
+    ],
+    setupFiles: ['./vitest.setup.ts'],
+    globals: true,
+  },
+})
